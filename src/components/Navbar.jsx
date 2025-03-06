289d9bf007096c4a79662ff0b83bec241b0a8b36
Mirav21 <miravsavaliya2004@gmail.com>
2025-03-06 06:28:02 +0530

    added event product form and landing page changes with attractive ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Sun, Moon, Monitor } from "lucide-react";
 import { useTheme } from "../theme/DarkMode";
 import Login from "../auth/Login";
 import Signup from "../auth/SignUp";
-import { Link, Navigate, useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
-
-const ThemeSwitcher = ({ currentTheme, onThemeChange }) => {
-  const themes = [
-    { name: "light", icon: Sun },
-    { name: "dark", icon: Moon },
-    { name: "system", icon: Monitor },
-  ];
-
-  const currentThemeData = themes.find((theme) => theme.name === currentTheme);
-
-  const getNextTheme = () => {
-    const currentIndex = themes.findIndex(
-      (theme) => theme.name === currentTheme
-    );
-    return themes[(currentIndex + 1) % themes.length].name;
-  };
-
-  return (
-    currentThemeData && (
-      <button
-        onClick={() => onThemeChange(getNextTheme())}
-        className="relative p-2.5 rounded-full bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 shadow-lg text-white flex items-center justify-center transition-all duration-300 hover:scale-110 active:scale-95 hover:shadow-xl focus:ring-3 focus:ring-blue-300 dark:focus:ring-blue-800 group"
-        title={`${
-          currentThemeData.name.charAt(0).toUpperCase() +
-          currentThemeData.name.slice(1)
-        } mode`}
-      >
-        <currentThemeData.icon className="w-6 h-6" />
-      </button>
-    )
-  );
-};
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -59,12 +26,12 @@ const Navbar = () => {
   useEffect(() => {
     //function which call every second
     const interval = setInterval(() => {
-      if (localStorage.getItem("Token")) {
+      if (localStorage.getItem("Token") && !isAdmin) {
         const decodedToken = jwtDecode(localStorage.getItem("Token"));
         if (decodedToken.exp * 1000 < Date.now()) {
           handleLogout();
         } else {
-          if (decodedToken.role === "ADMIN") {
+          if (decodedToken.roles === "ADMIN") {
             setIsAdmin(true);
           }
         }
@@ -165,20 +132,20 @@ const Navbar = () => {
               </button>
             </form>
 
-            {/* Navigation Links */}
-            {localStorage.getItem("Token") && isAdmin && (
-              <div className="flex items-center space-x-4">
-                <button
-                  onClick={() => redirect("addProducts")}
-                  className="px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-md shadow-md dark:bg-blue-500 dark:hover:bg-blue-600"
-                >
-                  Add Products
-                </button>
-              </div>
-            )}
-
             {/* Right Side Actions */}
             <div className="flex items-center space-x-4">
+              {/* Navigation Links */}
+              {localStorage.getItem("Token") && isAdmin && (
+                <div className="flex items-center space-x-4">
+                  <button
+                    onClick={() => redirect("addProduct")}
+                    className="px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-md shadow-md dark:bg-blue-500 dark:hover:bg-blue-600"
+                  >
+                    Add Products
+                  </button>
+                </div>
+              )}
+
               {localStorage.getItem("Token") ? (
                 <button
                   onClick={handleLogout}
@@ -194,7 +161,6 @@ const Navbar = () => {
                   Login/Register
                 </button>
               )}
-              <ThemeSwitcher currentTheme={theme} onThemeChange={setTheme} />
             </div>
           </div>
         </div>
